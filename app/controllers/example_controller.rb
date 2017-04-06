713d070b43f3130c3131177428e73022093ea06e
Chris Mckenzie <chrismckenzie@notonthehighstreet.com>
2017-04-06 14:08:37 +0200

    Add example 3

--- app/controllers/example_controller.rb
+++ app/controllers/example_controller.rb
@@ -4,10 +4,14 @@ class ExampleController < ApplicationController
     render :example_1
   end
 
-
   caches_action :example_2
   def example_2
     render :example_2
+  end
+
+  caches_action :example_3, layout: false
+  def example_3
+    render :example_3
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   get '/example/1', to: 'example#example_1'
   get '/example/2', to: 'example#example_2'
+  get '/example/3', to: 'example#example_3'
 end
